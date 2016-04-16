df13798be415f91b822f46d58169000ad087b17b
apleroy <apleroy@gmail.com>
2016-04-16 13:22:35 -0700

    list actions

--- examples/todomvc/actions/index.js
+++ examples/todomvc/actions/index.js
@@ -1,4 +1,16 @@
+//the action types are defined in a separate file
 import * as types from '../constants/ActionTypes'
+
+//These actions are the payloads of information that will send data from the
+//application to the 'store' which holds the universal state of the entire application.
+
+//The Action creators are the functions that create actions.  In redux, action creators return an action
+
+//good idea to pass as little data to each action as possible
+
+//actions for todo's and lists
+
+//todo actions
 
 export function addTodo(text) {
   return { type: types.ADD_TODO, text }
@@ -22,4 +34,27 @@ export function completeAll() {
 
 export function clearCompleted() {
   return { type: types.CLEAR_COMPLETED }
+}
+
+//LIST Actions
+export function addList(listtext) {
+  return {
+    type: types.ADD_LIST,
+    listtext
+  }
+}
+
+export function deleteList(id) {
+  return {
+    type: types.DELETE_LIST,
+    id
+  }
+}
+
+export function editList(id, listtext) {
+  return {
+    type: types.EDIT_LIST,
+    id,
+    listtext
+  }
 }
--- examples/todomvc/constants/ActionTypes.js
+++ examples/todomvc/constants/ActionTypes.js
@@ -1,6 +1,12 @@
+//Actions must have a type property that is typically defined as a string property
+
 export const ADD_TODO = 'ADD_TODO'
 export const DELETE_TODO = 'DELETE_TODO'
 export const EDIT_TODO = 'EDIT_TODO'
 export const COMPLETE_TODO = 'COMPLETE_TODO'
 export const COMPLETE_ALL = 'COMPLETE_ALL'
 export const CLEAR_COMPLETED = 'CLEAR_COMPLETED'
+
+export const ADD_LIST = 'ADD_LIST'
+export const DELETE_LIST = 'DELETE_LIST'
+export const EDIT_LIST = 'EDIT_LIST'
